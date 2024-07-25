f30d3f4a2dd06bd68f420b8fc12485ad8e996f9b
Harrison Tate <harritated@Harrisons-MacBook-Pro.local>
2024-07-25 16:44:29 -0600

    Adds a new command to create a board

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('createBoard', () =>{
+    cy.get('#number').type(3)
+    cy.get('#start').click()
+})
