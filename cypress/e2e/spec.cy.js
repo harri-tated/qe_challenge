a3679e6b8185f3a38080ede0f5187ed3dfafea78
Harrison Tate <harritated@Harrisons-MacBook-Pro.local>
2024-07-25 16:43:54 -0600

    Add the first spec and initial tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,32 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('TIC TAC TOE Tests', () => {
+  beforeEach(() => {
+    cy.visit('127.0.0.1:8080')
+  })
+
+  // afterEach(()=>{
+  //   cy.reload()
+  // })
+
+  it('Table will appear when value is entered', ()=>{
+    //Draw back with this test is that it will not cover 
+    //if the board is a valid Tic Tac Toe board.   
+    //Will need a test to check number of tiles
+    cy.get('#number').type(3)
+    cy.get('#start').should('exist').click()
+    cy.get('#table').should('be.visible')
+  })
+
+  it('After creating a table, the page can be reset', () => {
+    //Checks that we can clear the page to reset for a new game.
+    cy.get('#number').type(3)
+    cy.get('#start').click()
+    cy.reload()
+    cy.get('#number').should('not.have.value')
+    cy.get('#table').should('not.be.visible')
+  })
+
+  it('Third test', () =>{
+    cy.createBoard()
+    
   })
 })
